ccf4004f0ef3d798a93df653226655965fbe7e26
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2022-09-14 15:29:52 -0500

    Update checkout.sh
    Add sord

--- checkout.sh
+++ checkout.sh
@@ -2,11 +2,19 @@
 
 # check out EQquasi as the interseis component
 mkdir components
+mkdir bin
+
 cd components 
 git clone https://github.com/dunyuliu/EQquasi.git eqquasi
 cd eqquasi 
 source install-eqquasi.sh
+cp bin/eqquasi ../../bin/
 cd .. # back to components
 
-git clone https://github.com/wangyf/SORDlatest.git sord
-cd .. # back to components
+git clone https://github.com/wangyf/sordw3 sord
+cd sord
+cd src
+cp makefile_mpi makefile
+make
+cp sord-mO ../../../bin/
+cd ../../.. # back to components
